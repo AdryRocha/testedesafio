5323ec09d771aa6fd11edb8010025c5277356f5e
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-05-17 20:35:50 -0300

    Versionando o projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_renderizacao_server_side.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# 
+#### 
+
+``` bash
+```
+
+# Comandos iniciais:
+``` bash
+  
+```
+
+# Comandos git:
+``` bash
+  
+```
+####
+
+``` bash
+ 
+```
+
+# 
+``` bash
+ 
+```
